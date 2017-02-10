0128fe2f383c02e3108652d4765be34173acb48d
SparX97 <spaszahariev54@gmail.com>
2017-02-10 22:28:23 +0000

    Fisrt Commit

---
+++ Assets/Scripts/MovementControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour {
+
+    Transform myTransform;
+    //Rigidbody2D myRigidbody;
+    public float speed;
+
+    void Start () {
+        //myRigidbody = GetComponent<Rigidbody2D>();
+        myTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //myTransform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") ));
+        myTransform.Translate(new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime));
+        //myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, myRigidbody.velocity.y);
+        // Input.GetAxisRaw("Vertical") * speed * Time.deltaTime
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Collide");
+        
+    }
+
+
+}
