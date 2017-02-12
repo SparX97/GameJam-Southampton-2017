f422a56c6ec0a308639acaa198a350cfbf72ab9c
SparX97 <spaszahariev54@gmail.com>
2017-02-12 08:12:50 +0000

    8am

---
+++ Assets/Resources/Scripts/EnemyPersue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPersue : MonoBehaviour {
+
+    private Vector2 rotatePoint;
+    private Transform startPos;
+    private float playerClose = 10f;
+	// Use this for initialization
+	void Start () {
+        startPos = GetComponent<Transform>();
+        rotatePoint = new Vector2(startPos.position.x - 2f, startPos.position.y - 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(rotatePoint, Vector3.down, 2f);
+	}
+}
---
+++ Assets/Resources/Scripts/RunAway.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAway : MonoBehaviour {
+
+    private GameObject player;
+
+    private float range;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 dir = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+       range = Vector2.Distance(transform.position, player.transform.position);
+
+        //transform.Translate(new Vector2(player.transform.position.x * Time.deltaTime * 0.1f, player.transform.position.y * Time.deltaTime * 0.1f));
+        transform.Translate(Vector2.MoveTowards(transform.position, player.transform.position, range) * 0.3f * Time.deltaTime);
+
+    }
+}
